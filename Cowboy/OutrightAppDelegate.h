d05f8469bb3791ded3e8201422891a90effcc2fa
Julio Santos <julio@morgane.com>
2012-07-13 13:28:01 -0700

    Initial commit

---
+++ Cowboy/OutrightAppDelegate.h
@@ -0,0 +1,14 @@
+#import <Cocoa/Cocoa.h>
+#import "MenubarController.h"
+#import "PanelController.h"
+
+@interface OutrightAppDelegate : NSObject <NSApplicationDelegate, PanelControllerDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (nonatomic, strong) MenubarController *menubarController;
+@property (nonatomic, strong, readonly) PanelController *panelController;
+
+- (IBAction)togglePanel:(id)sender;
+
+@end
---
+++ Cowboy/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Cowboy
+//
+//  Created by Julio Santos on 7/13/12.
+//  Copyright (c) 2012 Outright. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+  return NSApplicationMain(argc, (const char **)argv);
+}
